aa86f4b075f8ad616b651f9d525ff1507a8ed6cb
Sergio Julio Garcia Urdiales <sjgu1@alu.ua.es>
2018-05-01 20:46:59 +0200

    Starting new activities

---
+++ MyApplication/app/src/main/java/sergiojuliogu/myapplication/SprintsActivity.java
@@ -0,0 +1,13 @@
+package sergiojuliogu.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SprintsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sprints);
+    }
+}
